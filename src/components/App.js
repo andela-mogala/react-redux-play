431383d4ba6f876f376de639baf45ce8be61367f
Michael Ogala <michael.ogala@andela.com>
2017-02-10 16:30:47 +0100

    create routes, root component, action, reducer and store and setup other boilerplate for the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import Header from './common/Header';
 
 class App extends React.Component {
   render() {
     return(
-      <div className='container-fluid'>
-        <p>Heaeder here...</p>
+      <div className="container-fluid">
+        <Header />
         {this.props.children}
       </div>
     );
   }
 }
 
-App.propTypes = {
-  children: PropTypes.object.isRequired
+App.ropTypes = {
+  children: React.PropTypes.object.isRequired
 };
 
 export default App;
