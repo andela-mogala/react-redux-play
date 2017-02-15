00b9116a0d4ae38b7bd7848a380d91467b454139
Michael Ogala <michael.ogala@andela.com>
2017-02-15 15:40:59 +0100

    handle errors and improve User Experience with feedback on asynchronous events

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import Header from './common/Header';
+import {connect} from 'react-redux';
 
 class App extends React.Component {
   render() {
     return(
       <div className="container-fluid">
-        <Header />
+        <Header loading={this.props.loading}/>
         {this.props.children}
       </div>
     );
   }
 }
 
-App.ropTypes = {
-  children: React.PropTypes.object.isRequired
+App.propTypes = {
+  children: React.PropTypes.object.isRequired,
+  loading: React.PropTypes.bool.isRequired
 };
 
-export default App;
+function mapStateToProps(state, ownProps) {
+  return {
+    loading: state.ajaxCallsInProgress > 0
+  };
+}
+
+export default connect(mapStateToProps)(App);
